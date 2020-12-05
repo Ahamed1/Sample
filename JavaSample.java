004cd6a837d35886b4b0d4026fd741a52cc340a1
Ahamed Sulaiman <ahamed@exmple.com>
2020-12-05 16:04:07 +1300

    test

---
+++ JavaSample.java
@@ -0,0 +1,13 @@
+
+
+public class JavaSample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stud
+	byte star;
+	
+	star=87;
+	System.out.println("this is close  "+ star);
+
+	}
+}
